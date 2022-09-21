525dd7ac1a8acc75e845fcaeb3757b141d029c2d
Nico Michel <nicomichel@utexas.edu>
2022-09-20 22:34:44 -0500

    Back to basics!
    Creating Nav for site, relearning

---
+++ personalsite/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+class Navbar extends React.Component{
+    render () {
+        return (
+            <div className='Nav'>
+                <p>Cup</p>
+            </div>
+        )
+    }
+}
+
+export default Navbar
