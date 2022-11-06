e3649141f5cd9ab3eff4f7b43acc96004c782ee6
Nico Michel <nicomichel@utexas.edu>
2022-11-05 21:45:35 -0500

    Coming back to this once more projects are done!

--- personalsite/src/components/Navbar1/index.js
+++ personalsite/src/components/Navbar1/index.js
@@ -5,7 +5,7 @@ class Navbar1 extends React.Component{
     render () {
         return (
                 <ul>
-                <li><a>Cup</a></li>
+                <li><a>Blog</a></li>
                 <li><a>Boxcar</a></li>
                 <li><a>Product</a></li>
                 </ul>
